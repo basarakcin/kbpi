c244584d8ed72bfd8bf7a38cc5929f34af91e7b5
Basar Akcin <33182504+basarakcin@users.noreply.github.com>
2023-06-26 18:57:34 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,6 +11,29 @@ for arg in "$@"; do
   extra_args="$extra_args $arg"
 done
 
+# if [[ $log_flag == true ]]; then
+#   echo "==================================================" | tee -a build.log
+#   echo "Starting new build: $(date)" | tee -a build.log
+#   echo "==================================================" | tee -a build.log
+
+#   docker build $extra_args --platform=linux/amd64 --progress=plain -t codesys:control . 2>&1 | tee -a build.log
+# else
+#   docker build $extra_args --platform=linux/amd64 -t codesys:control .
+# fi
+
+# Enable QEMU in Docker
+# This step makes Docker use the QEMU emulator when it runs containers
+# for architectures different from the host.
+# docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
+for arg in "$@"; do
+  if [[ $arg == "--log" ]]; then
+    log_flag=true
+  elif [[ $arg != "--platform=linux/amd64" ]]; then
+    extra_args="$extra_args $arg"
+  fi
+done
+
 if [[ $log_flag == true ]]; then
   echo "==================================================" | tee -a build.log
   echo "Starting new build: $(date)" | tee -a build.log
@@ -20,26 +43,3 @@ if [[ $log_flag == true ]]; then
 else
   docker build $extra_args --platform=linux/amd64 -t codesys:control .
 fi
-
-# Enable QEMU in Docker
-# This step makes Docker use the QEMU emulator when it runs containers
-# for architectures different from the host.
-# docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-
-# for arg in "$@"; do
-#   if [[ $arg == "--log" ]]; then
-#     log_flag=true
-#   elif [[ $arg != "--platform=linux/amd64" ]]; then
-#     extra_args="$extra_args $arg"
-#   fi
-# done
-
-# if [[ $log_flag == true ]]; then
-#   echo "==================================================" | tee -a build.log
-#   echo "Starting new build: $(date)" | tee -a build.log
-#   echo "==================================================" | tee -a build.log
-
-#   docker build $extra_args --platform=linux/amd64 --progress=plain -t codesys . 2>&1 | tee -a build.log
-# else
-#   docker build $extra_args --platform=linux/amd64 -t codesys .
-# fi

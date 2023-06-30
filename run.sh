d27fcb28f24830b90a655622c37605db1d8d3574
Basar Akcin <33182504+basarakcin@users.noreply.github.com>
2023-06-30 15:06:09 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker run -it -p 22:22 -p 11740:11740 -p 1217:1217 -p 11743:11743 --privileged codesys:control 
+docker run -it --rm -p 2222:22 -p 11740:11740 -p 1217:1217 -p 11743:11743 --privileged --name codesys_control codesys:control 
 
 

f32ea86d901ab40f2f062ca6a7e607b9cc7197f6
Basar Akcin <33182504+basarakcin@users.noreply.github.com>
2023-06-30 09:18:37 +0200

    Install codemeter, edge and c. control from .deb. Remove unzip requirement

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 SUDO=""
-# check if this linux system is debian based
 if ! which dpkg >/dev/zero; then 
 	echo "This install script only runs on debian based distributions"
 	exit 1
 fi
 
-# check if we are running as root, or we have sudoers access to dpkg
 if [ "$USER" != "root" ]; then
 	if ! which sudo >/dev/zero; then
 		echo "You are not running as root, and don't have sudo installed."
@@ -28,13 +26,6 @@ if ! which wget >/dev/zero; then
 	exit 1;
 fi
 
-if ! which unzip >/dev/zero; then
-	echo "We need the tool 'unzip' to unpack the CODESYS package."
-	echo "You may try:"
-	echo "  apt-get install unzip"
-	exit 1;
-fi
-
 if ! -d /lib64; then
 	echo "Seems that you are not running on a traditional multi arch system."
 	echo "As a workaround, you can link /lib to /lib64."
@@ -42,7 +33,7 @@ if ! -d /lib64; then
 fi
 
 if [ -z "${TRY_RUN}" ]; then
-	unzip -p /tmp/edge.package '*.deb'> /tmp/edge.deb && ${SUDO} dpkg -i /tmp/edge.deb
-    	unzip -p /tmp/codesys.package '*codemeter*.deb' > /tmp/codemeter.deb && ${SUDO} dpkg -i /tmp/codemeter.deb
-	unzip -p /tmp/codesys.package '*codesyscontrol*.deb' > /tmp/codesys.deb && ${SUDO} dpkg -i /tmp/codesys.deb
+	${SUDO} dpkg -i /tmp/*codemeter*.deb
+    	${SUDO} dpkg -i /tmp/*edge*.deb
+	${SUDO} dpkg -i /tmp/*control*.deb
 fi

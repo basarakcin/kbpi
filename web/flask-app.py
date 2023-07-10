700162538fc5893214d9a9713ab7a09c65f7b55d
Basar Akcin <33182504+basarakcin@users.noreply.github.com>
2023-07-10 16:30:45 +0200

    Update flask-app.py

--- web/flask-app.py
+++ web/flask-app.py
@@ -1,22 +1,42 @@
 from flask import Flask, jsonify
 import subprocess
 import logging
+import json
+import os
 
-logging.basicConfig(filename='/logs/flask-app.log', level=logging.INFO)
+logging.basicConfig(filename='flask-app.log', level=logging.INFO)
 
 app = Flask(__name__)
 
 @app.route('/logs', methods=['GET'])
 def get_logs():
-    container_name = "codesys_control"
+    container_name = "/codesys_control"
     logging.info('Fetching logs for container: %s', container_name)
-    result = subprocess.run(["docker", "logs", container_name], capture_output=True, text=True)
-    if result.returncode == 0:
-        logging.info('Successfully fetched logs for container: %s', container_name)
-        return jsonify(logs=result.stdout)
-    else:
-        logging.error('Failed to fetch logs for container: %s', container_name)
-        return jsonify(error=result.stderr), 500
+
+    container_id = None
+    for candidate in os.listdir('/var/lib/docker/containers'):
+        try:
+            with open(f'/var/lib/docker/containers/{candidate}/config.v2.json') as f:
+                config = json.load(f)
+            if config['Name'] == container_name:
+                container_id = candidate
+                break
+        except Exception as e:
+            logging.error('Failed to read config for candidate container id %s: %s', candidate, e)
+
+    if container_id is None:
+        logging.error('Could not find container with name: %s', container_name)
+        return jsonify(error='Could not find container'), 500
+
+    try:
+        with open(f'/var/lib/docker/containers/{container_id}/{container_id}-json.log') as f:
+            logs = f.read()
+    except Exception as e:
+        logging.error('Failed to read logs for container: %s', container_name)
+        return jsonify(error='Failed to read logs'), 500
+
+    logging.info('Successfully fetched logs for container: %s', container_name)
+    return jsonify(logs=logs)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')

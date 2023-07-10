cf6077102fb32505f5039da659692c52f889029e
Basar Akcin <33182504+basarakcin@users.noreply.github.com>
2023-07-10 14:35:47 +0200

    Update flask-app.py with logging

--- web/flask-app.py
+++ web/flask-app.py
@@ -1,15 +1,21 @@
 from flask import Flask, jsonify
 import subprocess
+import logging
+
+logging.basicConfig(filename='/logs/flask-app.log', level=logging.INFO)
 
 app = Flask(__name__)
 
 @app.route('/logs', methods=['GET'])
 def get_logs():
     container_name = "codesys_control"
+    logging.info('Fetching logs for container: %s', container_name)
     result = subprocess.run(["docker", "logs", container_name], capture_output=True, text=True)
     if result.returncode == 0:
+        logging.info('Successfully fetched logs for container: %s', container_name)
         return jsonify(logs=result.stdout)
     else:
+        logging.error('Failed to fetch logs for container: %s', container_name)
         return jsonify(error=result.stderr), 500
 
 if __name__ == '__main__':

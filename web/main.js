ca42974865ba1622cf7cb756da817b7c033b89a9
Basar Akcin <33182504+basarakcin@users.noreply.github.com>
2023-07-26 18:18:17 +0200

    Create main.js

---
+++ web/main.js
@@ -0,0 +1,48 @@
+window.onload = async function() {
+    // Variable to keep track of the logs
+    let logs = '';
+    let firstUpdate = true;
+    let logContainer = document.getElementById('logs');
+
+    // A helper function to handle the log data
+    function handleLogData(data) {
+        if (!firstUpdate) {
+            logs += '\n';  // append a newline character if not the first update
+        } else {
+            firstUpdate = false; // set firstUpdate to false after the first update
+        }
+        logs += data.replace(/\^@/g, '');
+        logContainer.innerHTML = ''; // Clear the current logs before appending new data
+        let pre = document.createElement('pre');
+        pre.textContent = logs;
+        logContainer.appendChild(pre);
+    }
+
+    try {
+        // Fetch current logs
+        let response = await fetch('http://localhost:5000/current_logs');
+        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+        let data = await response.text();
+        handleLogData(data);
+        console.log(data);
+
+        // Listen for updates
+        let eventSource = new EventSource('http://localhost:5000/logs');
+
+        eventSource.onopen = function(event) {
+            console.log('EventSource connection opened');
+        };
+
+        eventSource.onmessage = function(event) {
+            console.log('Received new logs:', event.data);
+            // Add new logs to the existing logs
+            handleLogData(event.data);
+        };
+
+        eventSource.onerror = function(error) {
+            console.error('EventSource encountered an error:', error);
+        };
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+}

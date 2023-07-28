99099fc55466c3e2fc7be1410a47609e2aeff9e9
Basar Akcin <33182504+basarakcin@users.noreply.github.com>
2023-07-28 16:21:53 +0200

    Update startup.sh

--- codesys/src/startup.sh
+++ codesys/src/startup.sh
@@ -2,24 +2,30 @@
 
 services=("ssh" "codesysedge" "codemeter")
 log_file="/var/log/codesys/output.log"
-username=$(whoami)
-hostname=$(hostname)
-current_dir=$(pwd)
 
-log_and_execute() {
-  echo "$username@$hostname:$current_dir \$" | sudo tee -a $log_file
-  "$@" 2>&1 | sudo tee -a $log_file
+# Executes and logs the command
+EXEC() {
+  username=$(whoami)
+  hostname=$(hostname)
+  current_dir=$(pwd)
+  if [ "$current_dir" = "$HOME" ]; then
+    current_dir="~"
+  fi
+
+  command=("$@")
+  echo "$username@$hostname:$current_dir\$ ${command[*]}" | sudo tee -a "$log_file"
+  "${command[@]}" 2>&1 | sudo tee -a "$log_file"
 }
 
 create_directory() {
-  log_and_execute sudo mkdir -p $1
+  EXEC sudo mkdir -p $1
 }
 
 create_directory /run/sshd
 
 service_start_status() {
-  log_and_execute sudo service $1 start
-  log_and_execute sudo service $1 status
+  EXEC sudo service $1 start
+  EXEC sudo service $1 status
 }
 
 for service in "${services[@]}"
@@ -29,7 +35,7 @@ done
 
 export LD_LIBRARY_PATH=/opt/codesys/lib
 
-log_and_execute sudo script -q -c "sudo /opt/codesys/bin/codesyscontrol.bin -d /etc/CODESYSControl.cfg" $log_file
+EXEC sudo /opt/codesys/bin/codesyscontrol.bin -d /etc/CODESYSControl.cfg
 
 # Not needed but keeping it just in case
 # # start tunnel to license server or start codemeter

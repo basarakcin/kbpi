2f4ca2b7fa9ccb4380cf842da43d215b85fbe74b
Basar Akcin <33182504+basarakcin@users.noreply.github.com>
2023-08-02 14:24:07 +0200

    Update flask-app.py

--- web/flask-app.py
+++ web/flask-app.py
@@ -2,10 +2,11 @@ from flask import Flask, Response
 from flask_cors import CORS
 import logging
 import time
+import shutil
 
 logging.basicConfig(filename='flask-app.log', level=logging.INFO, format='%(message)s')
 log_file_path = "/var/log/codesys/output.log"
-# log_file_path = "/var/log/codesys/codesyscontrol.log"
+control_log_path = "/var/log/codesys/codesyscontrol.log"
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})  # This allows CORS for all routes and origins
 
@@ -40,6 +41,19 @@ def get_current_logs():
 
     logging.info('Successfully fetched logs from file: %s', log_file_path)
     return Response(logs, mimetype='text/plain')
+
+@app.route('/append_logs', methods=['POST'])
+def append_logs():
+    logging.info('Appending logs from file: %s to %s', control_log_path, log_file_path)
+
+    try:
+        with open(control_log_path, 'rb') as src_file, open(log_file_path, 'ab') as dst_file:
+            shutil.copyfileobj(src_file, dst_file)
+        logging.info('Successfully appended logs from file: %s to %s', control_log_path, log_file_path)
+        return Response('Successfully appended logs', mimetype='text/plain')
+    except Exception as e:
+        logging.error('Failed to append logs from file: %s to %s', control_log_path, log_file_path)
+        return Response('Failed to append logs', mimetype='text/plain'), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')

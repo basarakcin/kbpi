4f7001bb4e645aefd9ee5aec0d62873a96105b74
Basar Akcin <33182504+basarakcin@users.noreply.github.com>
2023-08-08 14:37:32 +0200

    Update main.js

--- web/main.js
+++ web/main.js
@@ -4,6 +4,26 @@ window.onload = async function() {
     let infoContainer = document.getElementById('info'); // Dedicated container for info
     let logContainer = document.getElementById('logs');  // Container for logs
 
+     function formatLogRow(row) {
+        const columns = row.split(',');
+        const timestamp = columns[0];
+        const cmpId = columns[1];
+        const classId = columns[2];
+        const errorId = columns[3];
+        const infoId = columns[4];
+        let infoText = columns.slice(5).join(','); // Rest of the row, as info might contain commas
+
+        // Reformat XML-like tags
+        const match = infoText.match(/<(\w+)>([^<]+)<\/\1>/);
+        if (match) {
+            const tag = match[1];
+            const content = match[2];
+            infoText = tag + ": " + content;
+        }
+
+        return `${timestamp}, ${cmpId}, ${classId}, ${errorId}, ${infoId}, ${infoText}`;
+    }
+
     function handleLogData(data) {
         if (!firstUpdate) {
             logs += '\n';  // append a newline character if not the first update
@@ -16,7 +36,8 @@ window.onload = async function() {
         const rows = data.split('\n');
     
         rows.forEach(row => {
-            const columns = row.split(',');
+            const formattedRow = formatLogRow(row);
+            const columns = formattedRow.split(',');
     
             if (columns.length >= 5) {
                 const tr = document.createElement('tr');

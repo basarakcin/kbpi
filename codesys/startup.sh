7255b4bbff3512d8ea1db1e60e87e17bbccf978f
Basar Akcin <33182504+basarakcin@users.noreply.github.com>
2023-08-08 12:29:17 +0200

    Update startup.sh

--- codesys/startup.sh
+++ codesys/startup.sh
@@ -67,7 +67,7 @@ EXEC sudo /etc/init.d/ssh start &
 
 if [ -f /etc/init.d/codesyscontrol ]
 then
-sudo script -q -c "sudo /opt/codesys/bin/codesyscontrol.bin -d /etc/CODESYSControl.cfg" /var/opt/codesys/tmp.log
+sudo script -q -c "sudo /opt/codesys/bin/codesyscontrol.bin -d /etc/CODESYSControl.cfg" /var/opt/codesys/tmp.log | sudo tee -a /var/opt/codesys/tmp.log
 cat /var/opt/codesys/codesyscontrol.log >> /var/opt/codesys/tmp.log
 sudo mv /var/opt/codesys/tmp.log /var/opt/codesys/codesyscontrol.log
 # sudo script -q -c "sudo /opt/codesys/bin/codesyscontrol.bin -d /etc/CODESYSControl.cfg" /var/opt/codesys/log/codesyscontrol.log | sudo tee -a /var/opt/codesys/log/codesyscontrol.log

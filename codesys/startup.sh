fac6b1ca6548db4d57292befc188a39325588a25
Basar Akcin <33182504+basarakcin@users.noreply.github.com>
2023-08-30 11:06:15 +0200

    Update startup.sh

--- codesys/startup.sh
+++ codesys/startup.sh
@@ -9,20 +9,21 @@ log_with_timestamp() {
     done
 }
 
-# Executes and logs the command
 EXEC() {
-  username=$(whoami)
-  hostname=$(hostname)
-  current_dir=$(pwd)
+  local cmd=("$@")
+  local timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+  local username=$(whoami)
+  local hostname=$(hostname)
+  local current_dir=$(pwd)
+  
   if [ "$current_dir" = "$HOME" ]; then
     current_dir="~"
   fi
-
-  command=("$@")
-  timestamp=$(date "+%Y-%m-%d %H:%M:%S")
   
-  echo "[$timestamp] $username@$hostname:$current_dir\$ ${command[*]}" | sudo tee -a "$log_file"
-  "${command[@]}" 2>&1 | log_with_timestamp | sudo tee -a "$log_file"
+  echo "[$timestamp] $username@$hostname:$current_dir\$ ${cmd[*]}" | sudo tee -a "$log_file"
+  
+  # Execute the command, capture both stdout and stderr
+  "${cmd[@]}" 2>&1 | log_with_timestamp | sudo tee -a "$log_file"
 }
 
 if [[ -z `grep "docker0" /proc/net/dev` ]]; then
@@ -30,60 +31,47 @@ if [[ -z `grep "docker0" /proc/net/dev` ]]; then
   exit 143
 fi
 
-# SIGNAL-handler
 term_handler() {
-  if [ -f /etc/init.d/edgegateway ]
-  then
-    EXEC echo "Terminating CODESYS Edge Gateway ..."
+  if [ -f /etc/init.d/edgegateway ]; then
     EXEC /etc/init.d/codesysedge stop
   fi
 
-  if [ -f /etc/init.d/codesyscontrol ]
-  then
-    EXEC echo "Terminating CODESYS Runtime ..."
+  if [ -f /etc/init.d/codesyscontrol ]; then
     EXEC /etc/init.d/codesyscontrol stop
   fi
 
-  EXEC echo "Terminating ssh ..."
   EXEC /etc/init.d/ssh stop
-
-  # Delete logs when leaving the container.
   sudo rm /var/opt/codesys/codesyscontrol.log
   exit 143; # 128 + 15 -- SIGTERM
 }
 
-# On callback, stop all started processes in term_handler
 trap 'kill ${!}; term_handler' SIGINT SIGKILL SIGTERM SIGQUIT SIGTSTP SIGSTOP SIGHUP
 
 EXEC sudo mkdir -p /run/sshd
 
 if [ "$SSHPORT" ]; then
-  # User defined ssh port
   EXEC echo "The container binds the SSH server port to the configured port: $SSHPORT"
   EXEC sudo sed -i -e "s;#Port 22;Port $SSHPORT;" /etc/ssh/sshd_config
 else
   EXEC echo "The container binds the SSH server port to the default port: 2222"
 fi
 
-EXEC sudo /etc/init.d/ssh start &
+EXEC sudo /etc/init.d/ssh start
 
-if [ -f /etc/init.d/codesyscontrol ]
-then
-  sudo script -q -c "sudo /opt/codesys/bin/codesyscontrol.bin -d /etc/CODESYSControl.cfg" /var/opt/codesys/codesyscontrolinfo.log| sudo tee -a /var/opt/codesys/codesyscontrolinfo.log
+if [ -f /etc/init.d/codesyscontrol ]; then
+  EXEC sudo /opt/codesys/bin/codesyscontrol.bin -d /etc/CODESYSControl.cfg
 else
   EXEC echo "CODESYS runtime not installed. Download from here https://store.codesys.com/codesys-control-for-raspberry-pi-sl.html and install via CODESYS Development System."
 fi
 
-if [ -f /etc/init.d/codesysedge ]
-then
-  EXEC sudo /etc/init.d/codesysedge start >/dev/null &
+if [ -f /etc/init.d/codesysedge ]; then
+  EXEC sudo /etc/init.d/codesysedge start
 else
   EXEC echo "CODESYS Edge Gateway not installed. Download from here https://store.codesys.com/codesys-edge-gateway.html and install via CODESYS Development System."
 fi
 
 # Wait forever not to exit the container
-while true
-do
+while true; do
   tail -f /dev/null & wait ${!}
 done
 
